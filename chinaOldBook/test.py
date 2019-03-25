b00f85a586f478a5a696a9be61449a1659200376
wzf <469778348@qq.com>
2019-03-25 19:19:00 +0800

    写测试脚本创建关联数据库

--- chinaOldBook/test.py
+++ chinaOldBook/test.py
@@ -24,37 +24,80 @@ import pymysql
 db = pymysql.connect(host='localhost', port=3306, user='root', password='123456', db='hwss', charset='utf8')
 cursor = db.cursor()
 
-# delete_table = "DROP TABLE hw_chinaguji_bujiclassify"
+# delete_table4 = "DROP TABLE hw_chinaguji_content"
+# delete_table3 = "DROP TABLE hw_chinaguji_bookname"
+# delete_table2 = "DROP TABLE hw_chinaguji_bookclassify"
+# delete_table1 = "DROP TABLE hw_chinaguji_bujiclassify"
 # try:
-#     cursor.execute(delete_table)
+#     cursor.execute(delete_table4)
+#     print('删除章节内容表成功')
+#     cursor.execute(delete_table3)
+#     print('删除书籍名称表成功')
+#     cursor.execute(delete_table2)
+#     print('删除书籍分类表成功')
+#     cursor.execute(delete_table1)
 #     print('删除部级分类表成功')
 # except:
-#     print('删除部级分类表失败')
+#     print('删除表失败')
 
 create_table1_sql = """CREATE TABLE IF NOT EXISTS hw_chinaguji_bujiclassify (
-                    id INT NOT NULL AUTO_INCREMENT,
-                    buji_classify_name varchar(80) NOT NULL,
-                    PRIMARY KEY (id))
-                    ENGINE=InnoDB, DEFAULT CHARSET=utf8
-                    """
+                    buji_id INT(11) NOT NULL AUTO_INCREMENT COMMENT '部级编号ID',
+                    buji_classify_name varchar(80) NOT NULL COMMENT '部级名称',
+                    PRIMARY KEY (buji_id),
+                    INDEX(buji_id))
+                    ENGINE=InnoDB, CHARACTER SET utf8 COLLATE utf8_general_ci;"""
+
 try:
     cursor.execute(create_table1_sql)
     print('部级分类表创建成功')
 except:
     print('部级分类表创建失败')
 
-create_table2_sql = """CREATE TABLE IF NOT EXISTS hw_chinaguji_twoclassify (
-                    id INT NOT NULL AUTO_INCREMENT,
-                    twoclassify_name VARCHAR (100) NOT NULL,
-                    relation_table1_id INT NOT NULL ,
-                    PRIMARY KEY (id))
-                    ENGINE=InnoDB, DEFAULT CHARSET=utf8
-                    """
+
+create_table2_sql = """CREATE TABLE IF NOT EXISTS hw_chinaguji_bookclassify (
+                    bookclassify_id INT(11) NOT NULL AUTO_INCREMENT COMMENT '书籍分类编号ID',
+                    bookclassify_name VARCHAR (100) NOT NULL COMMENT '书籍分类名称',
+                    relation_bujiclassify_id INT(11) NOT NULL COMMENT '关联部级分类编号ID',
+                    PRIMARY KEY (bookclassify_id),
+                    FOREIGN KEY(relation_bujiclassify_id) REFERENCES hw_chinaguji_bujiclassify(buji_id) on delete cascade on update cascade)
+                    ENGINE=InnoDB CHARACTER SET utf8 COLLATE utf8_general_ci;"""
+
 try:
     cursor.execute(create_table2_sql)
     print('书籍分类表创建成功')
 except:
     print('书籍分类表创建失败')
+
+
+create_table3_sql = """CREATE TABLE IF NOT EXISTS hw_chinaguji_bookname (
+                    bookname_id INT(11) NOT NULL AUTO_INCREMENT COMMENT '书籍编号ID',
+                    bookname VARCHAR (200) NOT NULL COMMENT '书籍名称',
+                    relation_classify_id INT(11) NOT NULL COMMENT '关联书籍分类编号ID',
+                    PRIMARY KEY (bookname_id),
+                    FOREIGN KEY(relation_classify_id) REFERENCES hw_chinaguji_bookclassify(bookclassify_id) on delete cascade on update cascade)
+                    ENGINE=InnoDB CHARACTER SET utf8 COLLATE utf8_general_ci;"""
+
+try:
+    cursor.execute(create_table3_sql)
+    print('书籍名称表创建成功')
+except:
+    print('书籍名称表创建失败')
+
+
+create_table4_sql = """CREATE TABLE IF NOT EXISTS hw_chinaguji_content (
+                    content_id INT(11) NOT NULL AUTO_INCREMENT COMMENT '章节内容编号ID',
+                    chapter_name VARCHAR (300) NOT NULL COMMENT '章节名称',
+                    chapter_content LONGTEXT NOT NULL COMMENT '章节内容',
+                    relation_bookname_id INT(11) NOT NULL COMMENT '关联书籍编号ID',
+                    PRIMARY KEY (content_id),
+                    FOREIGN KEY(relation_bookname_id) REFERENCES hw_chinaguji_bookname(bookname_id) on delete cascade on update cascade)
+                    ENGINE=InnoDB CHARACTER SET utf8 COLLATE utf8_general_ci;"""
+
+try:
+    cursor.execute(create_table4_sql)
+    print('章节内容表创建成功')
+except:
+    print('章节内容表创建失败')
 
 
 
